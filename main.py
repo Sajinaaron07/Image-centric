c70bba25ad59e871e9887b372391c55aeb6d0b94
Brainless <55909054+parithipotter@users.noreply.github.com>
2023-01-29 21:24:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
+from fastapi import Body
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import openai
 
 app = FastAPI()
 load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 
@@ -27,16 +30,25 @@ cloudinary.config(
   
 
 
-
-
-
-
-
-
 @app.post("/")
 def upload(file:UploadFile = File(...)):
     result = cloudinary.uploader.upload(file.file)
     url = result.get('url')
     return {'url':url}
+
+@app.post("/chat")
+def chat(prompt:str= Body()):
+
+  response = openai.Completion.create(
+      prompt = prompt,
+      model = os.getenv('MODEL'),
+      max_tokens = 1000,
+      temperature = 0.9,
+
+  )
+  
+  
+  return response.choices[0]
+
 
    
